aa4a408f14b28b9834b160c9d05816e7b43d196c
Paul Kliem <paul@kliem.io>
2021-12-05 03:51:31 +0100

    Update world.js

--- world.js
+++ world.js
@@ -5,11 +5,13 @@ let isSpinning = true;
 
 // Made with Zdog
 
+var linkPoint = -60
+
 let earth = new Zdog.Illustration({
   element: '.zdog-canvas',
   dragRotate: true,
   resize: true,
-  zoom: 4,
+  zoom: 2,
 });
 
 // circle
@@ -17,39 +19,68 @@ let sphere = new Zdog.Shape({
   addTo: earth,
   diameter: 80,
   stroke: 100,
-  color: '#5792FF',
+  color: '#008AED',
 });
 
 //link 01
 let link1 = new Zdog.Shape({
   addTo: sphere,
   path: [
-    { z:   0},
-    { z:  -60},
+    { z:   linkPoint},
+    { z:  -110},
   ],
-  translate: { z: -90},
+  translate: { z: 0},
+  rotate: {x: 20},
   color: '#D9D9D9',
-  stroke: 4,
+  stroke: 2,
   fill: true,
 });
 
 new Zdog.Shape({
   addTo: link1,
-  translate: { z: -70},
+  translate: { z: -120},
   color: '#F5F5F5',
   stroke: 20,
   fill: true,
 });
 
-new Zdog.Shape({
+
+let link2 = new Zdog.Shape({
   addTo: sphere,
   path: [
-    { z:   0},
-    { z:  60},
+    { z:   linkPoint},
+    { z:  -130},
   ],
-  translate: { z: 90},
+  translate: { z: 0},
+  rotate: {x: 100, y: 50},
   color: '#D9D9D9',
-  stroke: 4,
+  stroke: 2,
+  fill: true,
+});
+
+let link3 = new Zdog.Shape({
+  addTo: sphere,
+  path: [
+    { z:   linkPoint},
+    { z:  -130},
+  ],
+  translate: { z: 0},
+  rotate: {x: 200, y: -50, z: 60},
+  color: '#D9D9D9',
+  stroke: 2,
+  fill: true,
+});
+
+let link4 = new Zdog.Shape({
+  addTo: sphere,
+  path: [
+    { z:   linkPoint},
+    { z:  -90},
+  ],
+  translate: { z: 0},
+  rotate: {x: 10, y: -200, z: -10},
+  color: '#D9D9D9',
+  stroke: 2,
   fill: true,
 });
 
@@ -59,5 +90,6 @@ function animate() {
 }
 
 animate();
+
 
 
